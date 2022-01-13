9220051a9fae8bd8c008659975a7a89875bfe66e
machz1988 <machz2012@gmail.com>
2022-01-13 16:17:05 -0600

    Adding some javascript to the project

---
+++ js/lastModified.js
@@ -0,0 +1,5 @@
+//let date = document.lastModified;
+
+//document.getElementById("currentDate").innerHTML = date;
+let date = document.lastModified;
+document.getElementById("currentDate").textContent = date;
