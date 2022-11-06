f9d2877b7d3fe9ebb938ed2b51ba6db1436c2b9d
machz1988 <machz2012@gmail.com>
2022-11-05 21:26:36 -0600

    Discover page first version

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -25,3 +25,24 @@ document.getElementById("lastModified").textContent = date;
 //document.getElementById("currentYear").innerHTML = 
 const currentYear = document.querySelector("#year")
 currentYear.textContent = now.getFullYear();
+
+// initialize display elements
+const lastVisitDisplay = document.querySelector(".lastVisit");
+
+// get the stored value in localStorage
+//let lastVisit = Number(window.localStorage.getItem("last-visit"));
+let dateLastVisit = Date(window.localStorage.getItem("date-last-visit"))
+
+// determine if this is the first visit or display the number of visits.
+if (dateLastVisit <= now) {
+    const diffTime = Math.abs(now - dateLastVisit);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+	lastVisitDisplay.textContent = diffDays;
+} else {
+	lastVisitDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+dateLastVisit = now;
+// store the new number of visits value
+window.localStorage.setItem("date-last-visit", dateLastVisit);
