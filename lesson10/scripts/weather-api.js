c64d0cdc0c953af0362be7351e81c9eb7fb280e3
machz1988 <machz2012@gmail.com>
2022-11-21 13:07:48 -0600

    Weather api practice

---
+++ lesson10/scripts/weather-api.js
@@ -0,0 +1,50 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=0874fda4b0c5e7c7d087937b085abaea";
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+}
+  
+apiFetch();
+
+function displayResults(weatherData){
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    const arr = desc.split(" ");
+    for (var i=0; i<arr.length; i++){
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    const str2 = arr.join(" ");
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = str2;
+}
+
+// derive the current date using a date object
+const now = new Date();
+
+//document.getElementById("currentDate").innerHTML = date;
+let date = document.lastModified;
+document.getElementById("lastModified").textContent = date;
+//document.getElementById("currentYear").innerHTML = 
+const currentYear = document.querySelector("#year")
+currentYear.textContent = now.getFullYear();
