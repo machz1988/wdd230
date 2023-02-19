341d0506ee64d500c8c9ea8e94ce275be5f4faae
machz1988 <machz2012@gmail.com>
2023-02-18 21:14:28 -0600

    Changing weather section classes for ids

--- chamber/scripts/wind-chill.js
+++ chamber/scripts/wind-chill.js
@@ -1,9 +1,9 @@
-const temp = parseInt(document.querySelector(".temp").textContent);
-const windSpeed = parseInt(document.querySelector(".wind-speed").textContent);
-const showWindChill = document.querySelector(".wind-chill");
+const temp = parseInt(document.querySelector("#temp").textContent);
+const windSpeed = parseInt(document.querySelector("#wind-speed").textContent);
+const showWindChill = document.querySelector("#wind-chill");
 const tempF = (temp*9/5)+32;
 const windSpeedMph = windSpeed/1.609;
-
+//console.log(`${tempF} ${windSpeedMph}`);
 if (tempF<=50 && windSpeedMph>3.0){
     let windChillF = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(windSpeedMph, 0.16) + 0.4275 * tempF * Math.pow(windSpeedMph, 0.16);
     let windChillC = (windChillF - 32) * 5 / 9;
